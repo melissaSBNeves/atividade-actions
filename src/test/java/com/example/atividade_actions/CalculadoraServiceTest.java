27788d86803dae69fe7278b09fce1697a3f1cd1c
melissaSNeves <145376881+MelissaSNeves@users.noreply.github.com>
2024-11-24 20:19:54 -0300

    Add tests

--- src/test/java/com/example/atividade_actions/CalculadoraServiceTest.java
+++ src/test/java/com/example/atividade_actions/CalculadoraServiceTest.java
@@ -2,6 +2,8 @@ package com.example.atividade_actions;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,5 +21,23 @@ class CalculadoraServiceTest {
         double resultado = calculadoraService.dividir(a, b);
         assertEquals(esperado, resultado);
     }
+
+    @Test
+    @DisplayName("Quando b for 0, então deve lançar ResponseStatusException com BAD_REQUEST")
+    public void testDividirErro() {
+        CalculadoraService calculadoraService = new CalculadoraService();
+        double a = 10;
+        double b = 0;
+
+        // Verifica se ResponseStatusException é lançada
+        ResponseStatusException exception = assertThrows(ResponseStatusException.class, () -> {
+            calculadoraService.dividir(a, b);
+        });
+
+        // Valida o status e a mensagem da exceção
+        assertEquals(HttpStatus.BAD_REQUEST, exception.getStatusCode());
+        assertEquals("Divisão por zero não permitida", exception.getReason());
+    }
+
 
 }
